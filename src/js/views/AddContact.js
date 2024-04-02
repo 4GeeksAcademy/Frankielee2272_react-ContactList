d5b6fdf97c96e64000624f924ba1127c120aa0c4
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-04-02 22:08:33 +0000

    Almost done

--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -3,20 +3,35 @@ import { Context } from "../store/appContext";
 
 export const AddContact = () => {
   const { store, actions } = useContext(Context);
-
-  const changeMikeName = () => {
-    actions.changeContactProperty("Mike A", "address", "toronto");
-  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  
   return (
     <div>
       <h1>Add Contact</h1>
-      {store.contacts.map((contact) => (
-        <div>
-          <span>{contact.name}</span>
-          {contact.address}
-        </div>
-      ))}
-      <button onClick={changeMikeName}>Change mike's address</button>
+      <form>
+  <div class="mb-3">
+    <label for="inputName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="inputName"/>
+  </div>
+  <div class="mb-3">
+    <label for="inputEmail" class="form-label">Email address</label>
+    <input onChange={(e)=>setEmail(e.target.value)} type="email" class="form-control" id="inputEmail"/>
+  </div>
+  <div class="mb-3">
+    <label for="inputPhone" class="form-label">Phone Number</label>
+    <input onChange={(e)=>setPhone(e.target.value)} type="tel" class="form-control" id="inputPhone"/>
+  </div>
+  <div class="mb-3">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input onChange={(e)=>setAddress(e.target.value)}type="text" class="form-control" id="inputAddress"/>
+  </div>
+ 
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 };
